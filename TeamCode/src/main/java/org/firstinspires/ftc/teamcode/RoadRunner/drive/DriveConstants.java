0e73855801d96bba1e98c66913e9f7459123ad27
matei8 <>
2021-11-18 13:58:35 +0200

    MaxVelocityTuner update2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/DriveConstants.java
@@ -87,7 +87,7 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
      
      */
-    public static double MAX_VEL = 39.421097316326424;
+    public static double MAX_VEL = 40;
     public static double MAX_ACCEL = 39.421097316326424;
     public static double MAX_ANG_VEL = Math.toRadians(360);
     public static double MAX_ANG_ACCEL = Math.toRadians(360);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/opmode/MaxVelocityTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/opmode/MaxVelocityTuner.java
@@ -35,7 +35,7 @@ public class MaxVelocityTuner extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        org.firstinspires.ftc.teamcode.drive.SampleTankDrive drive = new org.firstinspires.ftc.teamcode.drive.SampleTankDrive(hardwareMap);
 
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
